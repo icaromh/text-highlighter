d3a58953017681fa46c084ae4eb20f64edb30977
Ícaro Heimig <icaro.heimig@corp.globo.com>
2019-01-27 16:52:33 -0200

    feat: add serviceWorker with offline support

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const version = '0.2'
+const cacheName = `highlighter-${version}`
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/main.css',
+      '/bundle.js',
+    ])
+      .then(() => self.skipWaiting())),
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => response || fetch(event.request)),
+  )
+})
